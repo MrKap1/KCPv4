ca221902055b1364bdff190aa7753c7def58b951
MrKap1 <karolgrz32@gmail.com>
2025-12-09 21:44:11 -0600

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,14 @@
--- Generated with Durty Cloth Tool v3.1.0.3 (https://gta.clothing)
+-- Generated with Durty Cloth Tool v3.2.0.5 (https://gta.clothing)
 
 fx_version 'cerulean'
 game { 'gta5' }
 
 files {
-  'mp_m_freemode_01_mp_m_kcpv4_shop.meta',
-  'mp_f_freemode_01_mp_f_kcpv4_shop.meta',
+  'mp_m_freemode_01_mp_m_KCPv4_shop.meta',
+  'mp_f_freemode_01_mp_f_KCPv4_shop.meta',
   'pedalternatevariations.meta'
 }
 
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_kcpv4_shop.meta'
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_kcpv4_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_KCPv4_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_KCPv4_shop.meta'
 data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
